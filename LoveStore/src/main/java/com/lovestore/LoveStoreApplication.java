07139f97e31fa0074fe1b4a52f0dfd5caa6b474c
VuScriptMasterForge <vunhph33506@fpt.edu.vn>
2024-06-04 01:58:50 +0700

    Create project && upload database

---
+++ LoveStore/src/main/java/com/lovestore/LoveStoreApplication.java
@@ -0,0 +1,13 @@
+package com.lovestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveStoreApplication.class, args);
+    }
+
+}
---
+++ LoveStore/src/test/java/com/lovestore/LoveStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
